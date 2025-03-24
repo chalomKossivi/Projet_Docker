9567b4422b6b42a5543a99e4bce8ed0c9637891c
chalomKossivi <sd.cosmos.1812@gmail.com>
2025-03-24 16:56:14 +0100

    Projet

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const connectDB = require("./config/db");
+connectDB();
+
 
 const app = express();
 app.use(express.json());
